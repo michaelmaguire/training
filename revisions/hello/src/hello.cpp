8e6cb3339142f063fcd8ca16d72688fe05e8d411
Michael Maguire <github@michaelmaguire.ca>
2020-06-19 10:00:56 +0000

    Exercise 25.b
    conan@vm:~/training/revisions$ conan search hello/0.1@user/testing --revisions -r=artifactory
    Revisions for 'hello/0.1@user/testing' at remote 'artifactory':
    142922f37282be00d5dde7ef2b4065a9 (2020-06-19 10:00:01 UTC)
    c54d4535f9b8f1c8c55534450429e236 (2020-06-19 09:58:55 UTC)

--- revisions/hello/src/hello.cpp
+++ revisions/hello/src/hello.cpp
@@ -3,8 +3,8 @@
 
 void hello(){
     #ifdef NDEBUG
-    std::cout << "Hello World Release!" <<std::endl;
+    std::cout << "Hello World new rev Release!" <<std::endl;
     #else
-    std::cout << "Hello World Debug!" <<std::endl;
+    std::cout << "Hello World new rev Debug!" <<std::endl;
     #endif
 }
